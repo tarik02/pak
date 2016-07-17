291457e05b543476fc96b319711199ae184a32f7
Tarik02 <Taras.Fomin@gmail.com>
2016-07-17 09:23:32 +0300

    Initial Commit

---
+++ pak/PAK.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PAK
+{
+	public abstract class PAKException : Exception
+	{
+		public PAKException(String Message) : base(Message)
+		{
+
+		}
+	}
+
+	public class InvalidPAKIdException : PAKException
+	{
+		public InvalidPAKIdException() : base("Invalid PAK id")
+		{
+
+		}
+	}
+
+	public class InvalidPAKDirLengthException : PAKException
+	{
+		public InvalidPAKDirLengthException() : base("Invalid PAK dir length exception")
+		{
+
+		}
+	}
+
+
+	public class PAKFile
+	{
+		private struct PAKHeader
+		{
+			public String Id;
+			public uint DirOffset, DirLength;
+		}
+
+		private struct PAKEntry
+		{
+			public String Name;
+			public byte[] Data;
+		}
+
+
+		public const String PAKID = "PACK";
+		public const uint PAKHeaderSize = 4 + 4 + 4;
+		public const uint PAKEntrySize = 56 + 4 + 4;
+		private List<PAKEntry> Entries = new List<PAKEntry>();
+
+		private PAKFile()
+		{
+
+		}
+
+		
+
+		public void SaveToDirectory(String DirName)
+		{
+			SaveToDirectory(DirName, S => {  });
+		}
+
+		public void SaveToDirectory(String DirName, Action<String> FileCallback)
+		{
+			Directory.CreateDirectory(DirName);
+			
+			foreach (var Entry in Entries)
+			{
+				var FileName = DirName + Path.DirectorySeparatorChar + Entry.Name.Replace('/', Path.DirectorySeparatorChar);
+				var FileDir = FileName.Substring(0, FileName.LastIndexOf(Path.DirectorySeparatorChar));
+
+				FileCallback(FileName);
+
+
+				Directory.CreateDirectory(FileDir);
+
+				var Stream = File.Create(FileName, Entry.Data.Length);
+				Stream.Write(Entry.Data, 0, Entry.Data.Length);
+				Stream.Close();
+			}
+		}
+
+
+		public void SaveToStream(Stream Stream, Action<String> FileCallback, Action SavingCallback)
+		{
+			using (var Writer = new BinaryWriter(Stream))
+			{
+				Writer.Write(PAKID.ToCharArray());
+				Writer.Write((int)PAKHeaderSize);
+				Writer.Write((int)(Entries.Count * PAKEntrySize));
+
+				uint EntryWritePos = (uint)(PAKHeaderSize);
+				uint DataWritePos = (uint)(EntryWritePos + Entries.Count * PAKEntrySize);
+
+				foreach (var Entry in Entries)
+				{
+					FileCallback(Entry.Name);
+
+					Stream.Position = EntryWritePos;
+
+					var Chars = Entry.Name.ToCharArray();
+					Writer.Write(Chars);
+					Writer.Write(new byte[(EntryWritePos + 56) - Stream.Position]);
+
+					EntryWritePos += PAKEntrySize;
+
+
+					Writer.Write((int)DataWritePos);
+					Writer.Write((int)Entry.Data.Length);
+
+
+					Stream.Position = DataWritePos;
+					Writer.Write(Entry.Data);
+					DataWritePos += (uint)Entry.Data.Length;
+				}
+
+				SavingCallback();
+			}
+		}
+
+		public void SaveToPAK(string FileName, Action<String> FileCallback, Action SavingCallback)
+		{
+			SaveToStream(File.OpenWrite(FileName), FileCallback, SavingCallback);
+		}
+
+
+
+		public static PAKFile CreateFromFileStream(Stream Stream)
+		{
+			using (var Reader = new BinaryReader(Stream))
+			{
+				Stream.Position = 0;
+
+				var Header = new PAKHeader();
+
+				Header.Id = new String(Reader.ReadChars(4));
+				Header.DirOffset = (uint)Reader.ReadInt32();
+				Header.DirLength = (uint)Reader.ReadInt32();
+
+				if (Header.Id != PAKID)
+				{
+					throw new InvalidPAKIdException();
+				}
+
+				if (Header.DirLength % PAKEntrySize != 0)
+				{
+					throw new InvalidPAKDirLengthException();
+				}
+
+
+				Stream.Position = Header.DirOffset;
+
+
+				var EntriesCount = (uint)(Header.DirLength / PAKEntrySize);
+				var Result = new PAKFile();
+				
+				for (uint i = 0; i < EntriesCount; i++)
+				{
+					var Name = (new String(Reader.ReadChars(56))).Replace("\0", "");
+					var Offset = Reader.ReadInt32();
+					var Length = Reader.ReadInt32();
+
+					var OldPos = Stream.Position;
+					Stream.Position = Offset;
+
+					Result.Entries.Add(new PAKEntry()
+					{
+						Name = Name,
+						Data = Reader.ReadBytes(Length)
+					});
+
+					Stream.Position = OldPos;
+				}
+
+				return Result;
+			}
+		}
+
+		public static PAKFile CreateFromFile(String FileName)
+		{
+			return CreateFromFileStream(File.OpenRead(FileName));
+		}
+
+
+		public static PAKFile CreateFromDirectory(String DirName)
+		{
+			var Result = new PAKFile();
+
+			PackDirectory(Result, DirName);
+
+			return Result;
+		}
+
+		private static void PackDirectory(PAKFile Result, String DirName, String RelativePath = "")
+		{
+			foreach (var ASubDir in Directory.GetDirectories(DirName))
+			{
+				var SubDir = ASubDir.Substring(DirName.Length + 1);
+
+				PackDirectory(Result, ASubDir, Path.Combine(RelativePath, SubDir));
+			}
+
+			foreach (var ASubFile in Directory.GetFiles(DirName))
+			{
+				var SubFile = ASubFile.Substring(DirName.Length + 1);
+
+				Result.Entries.Add(new PAKEntry()
+				{
+					Name = Path.Combine(RelativePath, SubFile),
+					Data = File.ReadAllBytes(ASubFile)
+				});
+			}
+		}
+	}
+}
---
+++ pak/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PAK
+{
+	class Program
+	{
+		public static void Main(String[] Args)
+		{
+			new Program(new Queue<String>(Args));
+		}
+
+
+		public Program(Queue<String> Args)
+		{
+			if ((Args.Count < 1) || (Args.Count > 3))
+			{
+				PrintMessage("error.usage");
+				return;
+			}
+
+			var Mode = Args.Peek();
+
+			switch (Mode)
+			{
+			case "-pack":
+				Args.Dequeue();
+				Pack(Args);
+				break;
+			case "-unpack":
+				Args.Dequeue();
+				UnPack(Args);
+				break;
+			default:
+				if (Directory.Exists(Mode))
+				{
+					Pack(Args);
+				}
+				else if(File.Exists(Mode))
+				{
+					UnPack(Args);
+				}
+				else
+				{
+					PrintMessage("error.usage");
+				}
+				break;
+			}
+		}
+		
+
+		private void Pack(Queue<string> Args)
+		{
+			String Input = Args.Dequeue();
+			String Output = Input + ".pak";
+
+			try { Output = Args.Dequeue(); } catch {  }
+
+			if (!Directory.Exists(Input))
+			{
+				PrintMessage("pack.error.output-not-found", Input);
+				return;
+			}
+
+			try
+			{
+				PAK.PAKFile.CreateFromDirectory(Input).SaveToPAK(Output, FileName =>
+				{
+					PrintMessage("pack.process", FileName);
+				}, () =>
+				{
+					PrintMessage("pack.saving");
+				});
+
+				PrintMessage("pack.done");
+			}
+			catch (PAKException Exception)
+			{
+				PrintMessage("pack.error.custom", Exception);
+			}
+		}
+
+		private void UnPack(Queue<string> Args)
+		{
+			String Input = Args.Dequeue();
+			String Output = Input.Substring(0, Input.LastIndexOf("."));
+
+			try { Output = Args.Dequeue(); } catch {  }
+
+			if (!File.Exists(Input))
+			{
+				PrintMessage("unpack.error.input-not-found", Input);
+				return;
+			}
+
+			try
+			{
+				PAK.PAKFile.CreateFromFile(Input).SaveToDirectory(Output, FileName =>
+				{
+					PrintMessage("unpack.process", FileName);
+				});
+
+				PrintMessage("unpack.done");
+			}
+			catch (PAKException Exception)
+			{
+				PrintMessage("unpack.error.custom", Exception);
+			}
+		}
+
+		
+
+		private static Dictionary<String, String> Messages = new Dictionary<string, string>()
+		{
+			{ "error.usage", "Usage: -pack <input directory> [output file] or -unpack <input file> [output directory] or <input directory> [output file] or <input file> [output directory]" },
+
+
+			{ "unpack.error.input-not-found", "Input file {0} is not exists" },
+			{ "unpack.error.custom", "Error while unpacking: {0}" },
+
+			{ "unpack.process", "Unpacking file {0}..." },
+			{ "unpack.done", "Unpacking done" },
+
+
+			{ "pack.error.output-not-found", "Output file {0} is not exists" },
+			{ "pack.error.custom", "Error while packing: {0}" },
+
+			{ "pack.process", "Packing file {0}..." },
+			{ "pack.saving", "Saving output" },
+			{ "pack.done", "Packing done" },
+		};
+
+		private static String GetMessage(String Message)
+		{
+			Messages.TryGetValue(Message, out Message);
+
+			return Message;
+		}
+
+		private static void PrintMessage(String Message, params Object[] Args)
+		{
+			Console.WriteLine(String.Format(GetMessage(Message), Args));
+		}
+	}
+}
